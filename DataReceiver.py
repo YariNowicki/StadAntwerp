0c6449d983584dbc1cbd2049611bb733ffe61f86
YariNowicki <yari.nowicki@student.pxl.be>
2020-03-17 15:37:56 +0100

    Added working inputs

--- DataReceiver.py
+++ DataReceiver.py
@@ -2,12 +2,57 @@ import pandas as pd
 import os
 import snowflake.connector
 import json
+from sklearn import preprocessing #Used to pre process our data
 
 class DataReceiver:
     JSON_QUERY = "SELECT * FROM DEMO_ANTWERP_CITY.DEMO_DV_BV.GEO_POSTZONES"
     DF_QUERY = "SELECT * FROM DEMO_ANTWERP_CITY.DEMO_DV_BV.DISPLAY_SHAPES"
+    INPUT_QUERY = "SELECT * FROM DEMO_ANTWERP_CITY.DEMO_DV_BV.INPUT_DATA WHERE JAAR > 2005"
     DISPLAY_COLUMNS = ['postcode', 'jaar', 'fiets_naar_werk_school', 'naam', 'shape_area', 'shape_length']
-
+    COLUMNS = ['postcode', 'jaar', 'aantal_loontrekkenden', 'aantal_werkzoekende', 'aantal_zelfstandigen',
+               'inactieven', 'werkloosheidsdruk', 'belastingplichtigen',
+               'gemiddeld_netto_belastbaar_inkomen_per_persoon',
+               'opbrengst_aanvullende_personenbelasting_per_belast', 'totaal_netto_inkomen', 'aantal_inwoners',
+               'dichtheid',
+               'bibliotheek_bezocht', 'boek_gelezen', 'museum_bezocht', 'park_bezocht', 'restaurant_of_cafe_bezocht',
+               'televisie_gekeken',
+               'voorstelling_bezocht', 'sport_beoefend', 'tevredenheid_staat_wegen', 'tevreden_met_fiets_en_voetpaden',
+               'voldoende_bussen_trams', 'voldoende_parkeerplaatsen_voor_bewoners',
+               'regelmatig_de_fiets_naar_werk_school',
+               'regelmatig_te_voet_naar_werk_school', 'al_aso', 'al_bso', 'al_kso', 'al_tso', 'al_deel_bso',
+               'al_basis_binnen_a',
+               'al_basis_buiten_a', 'al_buso', 'al_so_geen_vertraging', 'al_so_vertraging', 'al_so_meer_vertraging',
+               'al_a_stroom',
+               'al_b_stroom', 'al_so_binnen_a', 'al_so_buiten_a', 'aantal_koten', 'kotdichtheid',
+               'plaatsen_buurtparkings', 'buurtparkings',
+               'fietsenstallingen', 'plaatsen_fietsenstallingen', 'sportterreinen', 'speelterreinen',
+               'opp_sportterreinen', 'opp_gebruiksgroen_en_pleinen',
+               'opp_speelterreinen', 'plaatsen_velo_stations', 'velo_stations']
+    COLS_CHANGED = ['bibliotheek_bezocht', 'boek_gelezen',
+                    'museum_bezocht',
+                    'park_bezocht',
+                    'restaurant_of_cafe_bezocht',
+                    'televisie_gekeken',
+                    'voorstelling_bezocht',
+                    'sport_beoefend',
+                    'regelmatig_de_fiets_naar_werk_school',
+                    'regelmatig_te_voet_naar_werk_school']
+    COLS_INPUTS = ['aantal_loontrekkenden', 'aantal_werkzoekende', 'aantal_zelfstandigen',
+                   'inactieven', 'werkloosheidsdruk', 'belastingplichtigen',
+                   'gemiddeld_netto_belastbaar_inkomen_per_persoon',
+                   'opbrengst_aanvullende_personenbelasting_per_belast', 'dichtheid',
+                   'bibliotheek_bezocht', 'boek_gelezen', 'museum_bezocht', 'park_bezocht',
+                   'restaurant_of_cafe_bezocht', 'televisie_gekeken',
+                   'voorstelling_bezocht', 'sport_beoefend',
+                   'regelmatig_te_voet_naar_werk_school', 'al_aso', 'al_bso', 'al_kso', 'al_tso', 'al_deel_bso',
+                   'al_basis_binnen_a',
+                   'al_basis_buiten_a', 'al_buso', 'al_so_geen_vertraging', 'al_so_vertraging', 'al_so_meer_vertraging',
+                   'al_a_stroom',
+                   'al_b_stroom', 'al_so_binnen_a', 'al_so_buiten_a', 'aantal_koten', 'kotdichtheid',
+                   'plaatsen_buurtparkings', 'buurtparkings',
+                   'fietsenstallingen', 'plaatsen_fietsenstallingen', 'sportterreinen', 'speelterreinen',
+                   'opp_sportterreinen', 'opp_gebruiksgroen_en_pleinen',
+                   'opp_speelterreinen', 'plaatsen_velo_stations', 'velo_stations']
     def get_geo_data(self):
         ctx = snowflake.connector.connect(
             user='YNOWICKI',
@@ -37,7 +82,7 @@ class DataReceiver:
             for k, v in links.items():
                 if postcode == v:
                     return k
-        cs.execute("SELECT * FROM DEMO_ANTWERP_CITY.DEMO_DV_BV.DISPLAY_SHAPES")
+        cs.execute(self.DF_QUERY)
         data = cs.fetchall()
 
         df = pd.DataFrame(data, columns=self.DISPLAY_COLUMNS)
@@ -47,10 +92,37 @@ class DataReceiver:
         df = df.fillna(0)
         df['id'] = range(1, 15)
 
-        cs.execute("SELECT * FROM DEMO_ANTWERP_CITY.DEMO_DV_BV.GEO_POSTZONES")
+        cs.execute(self.JSON_QUERY)
         j = cs.fetchall()
         d = json.loads(j[0][0])
 
         for feat in d['features']:
             feat['id'] = link(feat['properties']['postcode'])
         return df, d
+
+    def get_input_data(self):
+        ctx = snowflake.connector.connect(
+            user='YNOWICKI',
+            password=os.getenv('SNOWSQL_PWD'),
+            account='datasense.eu-west-1',
+            warehouse='SMALL_WH',
+            role='MOBILITY_ANTWERP',
+            database='DEMO_ANTWERP_CITY')
+        cs = ctx.cursor()
+        cs.execute(self.INPUT_QUERY)
+        data = cs.fetchall()
+        df = pd.DataFrame(data, columns=self.COLUMNS)
+        output = df['regelmatig_de_fiets_naar_werk_school']
+        output = output.str.replace(',', '.')
+        output = pd.to_numeric(output)
+        df = df.drop(['tevredenheid_staat_wegen', 'tevreden_met_fiets_en_voetpaden',
+                      'voldoende_bussen_trams', 'voldoende_parkeerplaatsen_voor_bewoners'], axis=1)
+        for col in self.COLS_CHANGED:
+            df[col] = df[col].str.replace(',', '.')
+            df[col] = pd.to_numeric(df[col])
+        min_max_scaler = preprocessing.MinMaxScaler() # puts everything in a range between 0 and 1
+        for col in self.COLS_INPUTS:
+            null_index = df[col].isnull()
+            df.loc[~null_index, [col]] = min_max_scaler.fit_transform(df.loc[~null_index, [col]])
+        df = df.fillna(-1)
+        return df
--- app.py
+++ app.py
@@ -9,8 +9,17 @@ import snowflake.connector
 import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
-from dash.dependencies import Output, Input
-
+from dash.dependencies import Output, Input, State
+columns = ['postcode', 'jaar', 'aantal_loontrekkenden','aantal_werkzoekende','aantal_zelfstandigen',
+        'inactieven','werkloosheidsdruk','belastingplichtigen','gemiddeld_netto_belastbaar_inkomen_per_persoon',
+        'opbrengst_aanvullende_personenbelasting_per_belast','totaal_netto_inkomen','aantal_inwoners','dichtheid',
+        'bibliotheek_bezocht','boek_gelezen','museum_bezocht','park_bezocht','restaurant_of_cafe_bezocht','televisie_gekeken',
+        'voorstelling_bezocht','sport_beoefend','regelmatig_de_fiets_naar_werk_school',
+        'regelmatig_te_voet_naar_werk_school','al_aso','al_bso','al_kso','al_tso','al_deel_bso','al_basis_binnen_a',
+        'al_basis_buiten_a','al_buso','al_so_geen_vertraging', 'al_so_vertraging','al_so_meer_vertraging','al_a_stroom',
+        'al_b_stroom','al_so_binnen_a','al_so_buiten_a','aantal_koten','kotdichtheid','plaatsen_buurtparkings','buurtparkings',
+        'fietsenstallingen','plaatsen_fietsenstallingen','sportterreinen','speelterreinen','opp_sportterreinen','opp_gebruiksgroen_en_pleinen',
+        'opp_speelterreinen','plaatsen_velo_stations','velo_stations']
 mapbox_token = "pk.eyJ1IjoieWFyaW5vd2lja2kiLCJhIjoiY2s3dTk4ZDV6MDE0dDNvbW93NXBjNTZ5bSJ9.6tGy4sJsG0DOBXsEiXmPEA"
 px.set_mapbox_access_token(mapbox_token)
 
@@ -24,21 +33,75 @@ def create_choropleth_mapbox(df, d):
 print("Getting data from snowflake...")
 snow = DataReceiver.DataReceiver()
 df, d = snow.get_geo_data()
+input_data = snow.get_input_data()
+print(len(input_data))
+input_data = input_data[input_data['jaar'] == 2015]
+print(len(input_data))
+input_data = input_data[input_data['postcode'] == '2000']
+print(len(input_data))
+input_data = input_data[snow.COLS_INPUTS]
+print(len(input_data))
 print("Succeeded")
-
+print(input_data)
 display_fig = go.Figure(create_choropleth_mapbox(df, d))
-
+for c in snow.COLS_INPUTS:
+    print(input_data[c].values[0])
 
 app = dash.Dash(__name__)
 app.layout = html.Div(children=[
     html.H1(children='Demo Antwerpen'),
+    dcc.Dropdown(id='add-input',
+    options=[
+        {'label': '2018', 'value': 2018},
+        {'label': '2017', 'value': 2017},
+        {'label': '2016', 'value': 2016}
+    ],value=2018),
+    html.Div(id='inputs', children=[],
+             style={
+                 'float': 'left',
+                 'width': '30%',
+                 'height': '500px',
+                 'overflow-y': 'scroll',
+                 'margin': '10px'
+            }
+    ),
 
     html.Div(
         dcc.Graph(id='districten',figure=display_fig, style={
-            'width': '30%'
+            'width': '30%',
+            'float': 'right'
         })
     )
+
 ])
+
+
+def create_todo_item(todo_text, todo_number):
+    return html.Div(
+        id='input-container-{}'.format(todo_number),
+        children=[
+            html.Span(todo_text),
+            dcc.Slider(
+                id='slider-{}'.format(todo_number),
+                min=0,
+                max=1.1,
+                step=0.01,
+                value=input_data[todo_text].values[0],
+            )
+        ]
+    )
+
+@app.callback(Output('inputs', 'children'),
+              [Input('add-input', 'value')],
+              [State('add-input', 'value'),
+               State('inputs', 'children')])
+def append_todo(n_enter_timestamp, todo_text, existing_todos):
+    for c in snow.COLS_INPUTS:
+        existing_todos.append(create_todo_item(
+            c, len(existing_todos)
+        ))
+    return existing_todos
+
 
 '''
 @app.callback(Output('districten', 'figure'))
