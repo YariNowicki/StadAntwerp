d2be86d53ba9246fef4d7eaed1fc9d6fb9287f1c
YariNowicki <yari.nowicki@student.pxl.be>
2020-03-17 14:01:32 +0100

    Cleaned up app.py, color on fietsgebruik

---
+++ DataReceiver.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import os
+import snowflake.connector
+import json
+
+class DataReceiver:
+    JSON_QUERY = "SELECT * FROM DEMO_ANTWERP_CITY.DEMO_DV_BV.GEO_POSTZONES"
+    DF_QUERY = "SELECT * FROM DEMO_ANTWERP_CITY.DEMO_DV_BV.DISPLAY_SHAPES"
+    DISPLAY_COLUMNS = ['postcode', 'jaar', 'fiets_naar_werk_school', 'naam', 'shape_area', 'shape_length']
+
+    def get_geo_data(self):
+        ctx = snowflake.connector.connect(
+            user='YNOWICKI',
+            password=os.getenv('SNOWSQL_PWD'),
+            account='datasense.eu-west-1',
+            warehouse='SMALL_WH',
+            role='MOBILITY_ANTWERP',
+            database='DEMO_ANTWERP_CITY')
+        cs = ctx.cursor()
+        def link(postcode):
+            links = {
+                1: 2000,
+                2: 2018,
+                3: 2020,
+                4: 2030,
+                5: 2040,
+                6: 2050,
+                7: 2060,
+                8: 2100,
+                9: 2140,
+                10: 2170,
+                11: 2180,
+                12: 2600,
+                13: 2610,
+                14: 2660
+            }
+            for k, v in links.items():
+                if postcode == v:
+                    return k
+        cs.execute("SELECT * FROM DEMO_ANTWERP_CITY.DEMO_DV_BV.DISPLAY_SHAPES")
+        data = cs.fetchall()
+
+        df = pd.DataFrame(data, columns=self.DISPLAY_COLUMNS)
+        df = df.sort_values(by=['postcode'])
+        df['fiets_naar_werk_school'] = df['fiets_naar_werk_school'].str.replace(',', '.')
+        df['fiets_naar_werk_school'] = pd.to_numeric(df['fiets_naar_werk_school'])
+        df = df.fillna(0)
+        df['id'] = range(1, 15)
+
+        cs.execute("SELECT * FROM DEMO_ANTWERP_CITY.DEMO_DV_BV.GEO_POSTZONES")
+        j = cs.fetchall()
+        d = json.loads(j[0][0])
+
+        for feat in d['features']:
+            feat['id'] = link(feat['properties']['postcode'])
+        return df, d
--- app.py
+++ app.py
@@ -3,61 +3,52 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 import json
+import os
+import DataReceiver
+import snowflake.connector
 import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 from dash.dependencies import Output, Input
-import urllib.request
-
-
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 mapbox_token = "pk.eyJ1IjoieWFyaW5vd2lja2kiLCJhIjoiY2s3dTk4ZDV6MDE0dDNvbW93NXBjNTZ5bSJ9.6tGy4sJsG0DOBXsEiXmPEA"
 px.set_mapbox_access_token(mapbox_token)
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+def create_choropleth_mapbox(df, d):
+    fig = px.choropleth_mapbox(data_frame=df, geojson=d, color='fiets_naar_werk_school', zoom=9,
+                        locations="id", featureidkey='properties.postcode',center={"lat":51.3, "lon": 4.4})
+    fig.update_geos(fitbounds="locations", visible=False)
+    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    return fig
 
-df = pd.read_csv('geodata/postzone.csv')
+print("Getting data from snowflake...")
+snow = DataReceiver.DataReceiver()
+df, d = snow.get_geo_data()
+print("Succeeded")
 
-with open('geodata/postzone.json') as json_file:
-    data = json.load(json_file)
+display_fig = go.Figure(create_choropleth_mapbox(df, d))
 
-fig = px.choropleth_mapbox(data_frame=df, geojson=data, color='naam',
-                    locations="id", featureidkey='properties.naam',center={"lat":51.2, "lon": 4.4}
-                    )
-fig.update_geos(fitbounds="locations", visible=False)
-fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
-display_fig = go.Figure(fig)
 
+app = dash.Dash(__name__)
 app.layout = html.Div(children=[
     html.H1(children='Demo Antwerpen'),
 
-    html.Div(children='''
-        First test
-    '''),
     html.Div(
-        dcc.Graph(id='r',figure=display_fig)
+        dcc.Graph(id='districten',figure=display_fig, style={
+            'width': '30%'
+        })
     )
-
-
-    # fig.show()
 ])
 
 '''
 @app.callback(Output('districten', 'figure'))
 def create_graph():
-    df = pd.read_csv('geodata/postzone.csv')
-    with open('D://Github//StadAntwerp//geodata//postzone.json') as json_file:
-        data = json.load(json_file)
-    fig = px.choropleth(data_frame=df, geojson=data,
-                        locations="naam", featureidkey="properties.postcode",
-                        projection="mercator"
-                        )
+    fig = px.choropleth_mapbox(data_frame=df, geojson=d,
+                               locations="id", featureidkey='properties.postcode', center={"lat": 51.2, "lon": 4.4}
+                               )
     fig.update_geos(fitbounds="locations", visible=False)
-    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0}, mapbox=mapbox_token)
+    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
     display_fig = go.Figure(fig)
-    display_fig.show()
     return display_fig
 '''
 
