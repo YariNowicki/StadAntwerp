4c44fbac7265be060b47a1bf6b536631c5522fcb
Yari Nowicki <yari.nowicki@student.pxl.be>
2020-05-13 12:14:17 +0200

    added prints

--- SQL/5_SAT_TEMP_TGT.sql
+++ SQL/5_SAT_TEMP_TGT.sql
@@ -1,19 +1,3 @@
-/*
-    ___  ___  ______  |  
-   / _ \/ _ \|  ____| |  agile    
-  / / \ \/ \ \ |__    |  automation
- / /  /\ \  \ \ __|   |  factory   
-/_/  /_/\_\  \_\      | 
-
-Vaultspeed version: 4.1.14.0, generation date: 2020/05/12 12:48:20
-DV_NAME: DEMO_ANTWERP_CITY_DATA_VAULT - Release: 2.2 predictions schema(2.2) - Comment: Predictions schema - Release date: 2020/05/12 12:44:25, 
-BV release: init(1) - Comment: initial release - Release date: 2020/05/12 10:45:49, 
-SRC_NAME: PRED - Release: PRED(2) - Comment: Update schema parameters - Release date: 2020/05/12 12:30:07
- */
-
-
--- SAT_TEMP_TGT
-
 TRUNCATE TABLE "PRED_STG"."SAT_PRED_PREDICTIONS_TMP";
 
 INSERT INTO "PRED_STG"."SAT_PRED_PREDICTIONS_TMP"(
